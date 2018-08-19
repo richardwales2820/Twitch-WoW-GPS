fef8be399dcd39ef42d5c20fdc1ded91439c8d7c
Richie Wales <richard_wales@knights.ucf.edu>
2018-08-19 01:10:43 -0700

    Grab whole screen as well, pivot

--- stalker.py
+++ stalker.py
@@ -15,17 +15,25 @@ class Stream:
             "-vcodec", "rawvideo", "-"],
             stdin=sp.PIPE, stdout=sp.PIPE)
     
-    def read_frame(self):
+    def read_default_map_location(self):
         raw_image = self.pipe.stdout.read(1280 * 720 * 3)
         frame = np.fromstring(raw_image, dtype='uint8').reshape((720, 1280, 3))
         cropped_location = frame[0:20:, -150:-10]
 
         return cv2.cvtColor(cropped_location, cv2.COLOR_BGR2RGB)
 
-soda = Stream('https://www.twitch.tv/sodapoppin')
-cv2.imshow('cropped_image', soda.read_frame())
+    def read_frame(self):
+        raw_image = self.pipe.stdout.read(1280 * 720 * 3)
+        frame = np.fromstring(raw_image, dtype='uint8').reshape((720, 1280, 3))
 
-other = Stream('https://www.twitch.tv/anthony_kongphan')
-cv2.imshow('other_cropped', other.read_frame())
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+stream = Stream('https://www.twitch.tv/sodapoppin')
+
+frame = stream.read_frame()
+map_frame = stream.read_default_map_location()
+
+cv2.imshow('Whole_Image', frame)
+cv2.imshow('Map_Image', map_frame)
 
 cv2.waitKey(0)
